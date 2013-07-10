db9053c70a344b5e6dd771cc4ffafadd3ff4fe11
Hemendra Kumar <hemendra.chaudhary619@gmail.com>
2013-07-10 11:30:56 +0530

    will fetch video code from iframe code also.
    tested to be working for youtube shorten url, complete url,iframe code and vimeo codes(both iframe and link).

--- Yiitube.php
+++ Yiitube.php
@@ -11,132 +11,130 @@
  * @author Nicola Puddu
  * @author Sébastien Monterisi <sebastienmonterisi@yahoo.fr>
  * @author Daniel Sánchez <sanchez.p.dani@gmail.com>
+ * @author Hemendra Chaudhary <hemendra.chaudhary619@gmail.com>
  */
 class Yiitube extends CWidget {
-    
     /**
-     * Possible status of the widget 
-     * @var int Widget status
+     * these constants rappresent the possible status of the widget
+     * @var int
      */
+
     const OK = 0;
     const NO_VIDEO_CODE = 1;
     const NO_VIDEO_CODE_IN_URL = 2;
     const NO_SUPPORTED_PLAYER = 3;
-        
     /**
-     * Available video hosting services players
-     * @var string Player name
+     * these constants rappresent the available players
+     * @var string the player name
      */
-    const SERVICE_YOUTUBE = 'youtube';
-    const SERVICE_MEGAVIDEO = 'megavideo';
-    const SERVICE_VIMEO = 'vimeo';
-    const SERVICE_VEOH = 'veoh';
-    const SERVICE_DAILYMOTION = 'dailymotion';
-    
+    const YOUTUBE = 'youtube';
+    const MEGAVIDEO = 'megavideo';
+    const VIMEO = 'vimeo';
+    const VEOH = 'veoh';
+
     /**
-     * @var array Possible standard heights of a youtube video
+     * @var array possible standard heights of a youtube video
      */
     private $_height = array(
-                        'youtube'=>array(
-                            'small' => 349,
-                            'normal' => 390,
-                            'big' => 510,
-                            'huge' => 750,
-                        ),
-                        'megavideo'=>array(
-                            'small' => 330,
-                            'normal' => 344,
-                            'big' => 430,
-                            'huge' => 551,
-                        ),
-                        'vimeo'=>array(
-                            'small' => 225,
-                            'normal' => 360,
-                            'big' => 450,
-                            'huge' => 576
-                        ),
-                        'veoh'=>array(
-                            'small' => 340,
-                            'normal' => 510,
-                            'big' => 680,
-                            'huge' => 850,
-                        ),
-                        'dailymotion'=>array(
-                            'small' => 180,
-                            'normal' => 270,
-                            'big' => 315,
-                            'huge' => 576,
-                        ),
-                    );
+        'youtube' => array(
+            'thumb'=>210,
+            'small' => 349,
+            'normal' => 390,
+            'big' => 510,
+            'huge' => 750,
+        ),
+        'megavideo' => array(
+            'thumb' => 330,
+            'small' => 349,
+            'normal' => 344,
+            'big' => 430,
+            'huge' => 551,
+        ),
+        'vimeo' => array(
+            'thumb' => 225,
+            'small' => 349,
+            'normal' => 360,
+            'big' => 450,
+            'huge' => 576
+        ),
+        'veoh' => array(
+            'thumb' => 340,
+            'small' => 349,
+            'normal' => 510,
+            'big' => 680,
+            'huge' => 850,
+        ),
+    );
+
     /**
      * @var array possible standard widths of a youtube video
      */
     private $_width = array(
-                        'youtube'=>array(
-                            'small' => 560,
-                            'normal' => 640,
-                            'big' => 853,
-                            'huge' => 1280,
-                        ),
-                        'megavideo'=>array(
-                            'small' => 450,
-                            'normal' => 640,
-                            'big' => 800,
-                            'huge' => 1024,
-                        ),
-                        'vimeo'=>array(
-                            'small' => 400,
-                            'normal' => 640,
-                            'big' => 800,
-                            'huge' => 1024,
-                        ),
-                        'veoh'=>array(
-                            'small' => 410,
-                            'normal' => 615,
-                            'big' => 820,
-                            'huge' => 1025,
-                        ),
-                        'dailymotion'=>array(
-                            'small' => 320,
-                            'normal' => 480,
-                            'big' => 560,
-                            'huge' => 1024,
-                        ),
-                    );
+        'youtube' => array(
+            'thumb' => 280,
+            'small' => 560,
+            'normal' => 640,
+            'big' => 853,
+            'huge' => 1280,
+        ),
+        'megavideo' => array(
+            'thumb' => 450,
+            'small' => 560,
+            'normal' => 640,
+            'big' => 800,
+            'huge' => 1024,
+        ),
+        'vimeo' => array(
+            'thumb' => 400,
+            'small' => 560,
+            'normal' => 640,
+            'big' => 800,
+            'huge' => 1024,
+        ),
+        'veoh' => array(
+            'thumb' => 410,
+            'small' => 560,
+            'normal' => 615,
+            'big' => 820,
+            'huge' => 1025,
+        ),
+    );
+
     /**
      * @var int the current status. possible values are those of the constants
      */
     private $_status;
-    
+
     /**
-     * If no player is specified when calling this widget, it will try to look for the
-     * correct player inside the url passed through $v (it only works when the full url
-     * is provided, if only the video code is provided, then the player parameter is
-     * mandatory, otherwise an exception will be raised
      * @var string the player to use to display the video
      */
-    public $player;
+    //public $player = 'youtube';
+
     /**
      * when setting this parameter you can either use the video code or the whole url.
      * @var string the youtube video you want to display.
      */
     public $v;
+    public $player='youtube';
     /**
      * @var string size options for the video
      * @example small, normal, big, huge
      */
     public $size = 'normal';
+
     /**
      * @var bool if you want to display the video in HD or not. Defaults to false.
      */
     public $hd = false;
     
+    
+    public $iframe=false;
+
     /**
      * check the integrity of the video code when initialized
      * @see CWidget::init()
      */
-    public function init()
-    {
+    public function init() {
         parent::init();
         // set default status
         $this->_status = self::OK;
@@ -144,9 +142,20 @@ class Yiitube extends CWidget {
         $this->playerSupport();
         // check the video code
         if ($this->v != NULL) {
-            if (filter_var($this->v, FILTER_VALIDATE_URL))
+            //echo "here v not null";exit;
+            if (filter_var($this->v, FILTER_VALIDATE_URL)){
+                //echo "here url validated";exit;
+                if(strpos($this->v, 'youtu.be')){
+                   $this->iframe=true;
+                }
                 $this->v = $this->getVideoCode();
-        } else
+            }else{
+                if(strpos($this->v, 'iframe') || strpos($this->v, 'youtu.be')){
+                   $this->iframe=true;
+                }
+            }
+        }
+        else
             $this->_status = self::NO_VIDEO_CODE;
     }
 
@@ -154,174 +163,127 @@ class Yiitube extends CWidget {
      * display the video
      * @see CWidget::run()
      */
-    public function run()
-    {
+    public function run() {
         if ($this->_status === self::OK) {
-            $method = strtolower($this->player).'Code';
+            $method = strtolower($this->player) . 'Code';
             echo $this->$method();
-        } else
-            echo '<b>'.$this->renderError().'</b>';
+        }
+        else
+            echo '<b>' . $this->renderError() . '</b>';
     }
-    
+
     /**
      * checks if yiitube is supporting the desired player
-     * if no player setted, try to look for the player in the url
-     * if no url is provided, player parameter is mandatory
      * @return bool
      */
-    protected function playerSupport()
-    {    
-        //Looking for the video player in the url
-        if (empty($this->player)){
-            if (stristr($this->v, self::SERVICE_YOUTUBE)) {
-                $this->player = self::SERVICE_YOUTUBE;
+    protected function playerSupport() {
+        switch (strtolower($this->player)) {
+            case self::YOUTUBE:
                 return true;
                 break;
-            }elseif(stristr($this->v, self::SERVICE_MEGAVIDEO)) {
-                $this->player = self::SERVICE_MEGAVIDEO;
+            case self::MEGAVIDEO:
                 return true;
                 break;
-            }elseif(stristr($this->v, self::SERVICE_VIMEO)) {
-                $this->player = self::SERVICE_VIMEO;
+            case self::VIMEO:
                 return true;
                 break;
-            }elseif(stristr($this->v, self::SERVICE_VEOH)) {
-                $this->player = self::SERVICE_VEOH;
+            case self::VEOH:
                 return true;
                 break;
-            }elseif(stristr($this->v, self::SERVICE_DAILYMOTION)) {
-                $this->player = self::SERVICE_DAILYMOTION;
-                return true;
-                break;
-            }else{
+            default:
                 $this->_status = self::NO_SUPPORTED_PLAYER;
                 return false;
                 break;
-            }
-        //If player parameter is not empty, we will check if Yiitube supports
-        //the desired player        
-        }else{
-            switch (strtolower($this->player)) {
-                case self::SERVICE_YOUTUBE:
-                    return true;    
-                    break;
-                case self::SERVICE_MEGAVIDEO:
-                    return true;
-                    break;
-                case self::SERVICE_VIMEO:
-                    return true;
-                    break;
-                case self::SERVICE_VEOH:
-                    return true;
-                    break;
-                case self::SERVICE_DAILYMOTION:
-                    return true;
-                    break;
-                default:                
-                    $this->_status = self::NO_SUPPORTED_PLAYER;
-                    return false;
-                    break;
-            }
         }
     }
-    
+
     /**
      * @return string the youtube iframe according to the settings
      */
-    protected function youtubeCode()
-    {
-        return <<<SERVICE_YOUTUBE
-        <iframe title="YouTube video player" width="{$this->_width[self::SERVICE_YOUTUBE][$this->size]}" height="{$this->_height[self::SERVICE_YOUTUBE][$this->size]}" 
-        src="{$this->youtubeVideoURL()}" frameborder="0" allowfullscreen></iframe>
-SERVICE_YOUTUBE;
+    protected function youtubeCode() {
+        return <<<YOUTUBE
+    	<iframe title="YouTube video player" width="{$this->_width[self::YOUTUBE][$this->size]}" height="{$this->_height[self::YOUTUBE][$this->size]}" 
+		src="{$this->youtubeVideoURL()}" frameborder="0" allowfullscreen></iframe>
+YOUTUBE;
     }
-    
+
     /**
      * @return string the megavideo object code according to the settings
      */
-    protected function megavideoCode()
-    {
-        return <<<SERVICE_MEGAVIDEO
-        <object width="{$this->_width[self::SERVICE_MEGAVIDEO][$this->size]}" height="{$this->_height[self::SERVICE_MEGAVIDEO][$this->size]}">
-            <param name="movie" value="http://www.megavideo.com/v/{$this->v}"></param>
-            <param name="allowFullScreen" value="true"></param>
-            <embed src="http://www.megavideo.com/v/{$this->v}" type="application/x-shockwave-flash" allowfullscreen="true" width="{$this->_width[self::SERVICE_MEGAVIDEO][$this->size]}" height="{$this->_height[self::SERVICE_MEGAVIDEO][$this->size]}"></embed>
-        </object>
-SERVICE_MEGAVIDEO;
+    protected function megavideoCode() {
+        return <<<MEGAVIDEO
+		<object width="{$this->_width[self::MEGAVIDEO][$this->size]}" height="{$this->_height[self::MEGAVIDEO][$this->size]}">
+			<param name="movie" value="http://www.megavideo.com/v/{$this->v}"></param>
+			<param name="allowFullScreen" value="true"></param>
+			<embed src="http://www.megavideo.com/v/{$this->v}" type="application/x-shockwave-flash" allowfullscreen="true" width="{$this->_width[self::MEGAVIDEO][$this->size]}" height="{$this->_height[self::MEGAVIDEO][$this->size]}"></embed>
+		</object>
+MEGAVIDEO;
     }
-    
+
     /**
      * @return string the vimeo iframe code according to the settings
      */
-    protected function vimeoCode()
-    {
-        return <<<SERVICE_VIMEO
-        <iframe src="http://player.vimeo.com/video/{$this->v}" width="{$this->_width[self::SERVICE_VIMEO][$this->size]}" height="{$this->_height[self::SERVICE_VIMEO][$this->size]}" frameborder="0"></iframe>
-SERVICE_VIMEO;
+    protected function vimeoCode() {
+        return <<<VIMEO
+		<iframe src="http://player.vimeo.com/video/21339058?byline=0&amp;portrait=0" width="{$this->_width[self::VIMEO][$this->size]}" height="{$this->_height[self::VIMEO][$this->size]}" frameborder="0"></iframe>
+VIMEO;
     }
-    
+
     /**
      * @return string the veoh code according to the settings
      */
-    protected function veohCode()
-    {
-        return <<<SERVICE_VEOH
-        <object width="{$this->_width[self::SERVICE_VEOH][$this->size]}" height="{$this->_height[self::SERVICE_VEOH][$this->size]}" id="veohFlashPlayer" name="veohFlashPlayer">
-            <param name="movie" value="http://www.veoh.com/static/swf/veoh/MediaPlayerWrapper.swf?version=&permalinkId={$this->v}&player=videodetailsembedded&videoAutoPlay=0&id=anonymous"></param>
-            <param name="allowFullScreen" value="true"></param>
-            <param name="allowscriptaccess" value="always"></param>
-            <embed src="http://www.veoh.com/static/swf/veoh/MediaPlayerWrapper.swf?version=&permalinkId={$this->v}&player=videodetailsembedded&videoAutoPlay=0&id=anonymous" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="{$this->_width[self::SERVICE_VEOH][$this->size]}" height="{$this->_height[self::SERVICE_VEOH][$this->size]}" id="veohFlashPlayerEmbed" name="veohFlashPlayerEmbed"></embed>
-        </object>
-SERVICE_VEOH;
+    protected function veohCode() {
+        return <<<VEOH
+		<object width="{$this->_width[self::VEOH][$this->size]}" height="{$this->_height[self::VEOH][$this->size]}" id="veohFlashPlayer" name="veohFlashPlayer">
+			<param name="movie" value="http://www.veoh.com/static/swf/veoh/MediaPlayerWrapper.swf?version=&permalinkId={$this->v}&player=videodetailsembedded&videoAutoPlay=0&id=anonymous"></param>
+			<param name="allowFullScreen" value="true"></param>
+			<param name="allowscriptaccess" value="always"></param>
+			<embed src="http://www.veoh.com/static/swf/veoh/MediaPlayerWrapper.swf?version=&permalinkId={$this->v}&player=videodetailsembedded&videoAutoPlay=0&id=anonymous" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="{$this->_width[self::VEOH][$this->size]}" height="{$this->_height[self::VEOH][$this->size]}" id="veohFlashPlayerEmbed" name="veohFlashPlayerEmbed"></embed>
+		</object>
+VEOH;
     }
-        
-        /**
-         * Generate the Dailymotion player code
-         * 
-         * @return string Dailymotion html code
-         */
-        protected function dailymotionCode()
-        {
-            return <<<CODESET
-                <iframe frameborder="0" 
-                    width="{$this->_width[self::SERVICE_DAILYMOTION][$this->size]}" 
-                    height="{$this->_height[self::SERVICE_DAILYMOTION][$this->size]}" 
-                    src="http://www.dailymotion.com/embed/video/{$this->v}">
-                </iframe>
-CODESET;
-        }
-     
+
     /**
      * @return string the video url that has to be used inside the iframe
      */
-    protected function youtubeVideoURL()
-    {
-        $url = "http://www.youtube.com/embed/{$this->v}?rel=0";
+    protected function youtubeVideoURL() {
+        if ($this->iframe) {
+            
+            preg_match('#(\.be/|/embed/|/v/|/watch\?v=)([A-Za-z0-9_-]{5,11})#', $this->v, $match);
+            $id = $match[2];
+            $url = "http://www.youtube.com/embed/{$id}?rel=0";
+        } else {
+            $url = "http://www.youtube.com/embed/{$this->v}?rel=0";
+        }
+
         if ($this->hd)
             $url .= '&amp;hd=1';
         return $url;
     }
-    
+
     /**
      * search into the provided url for the player video code.
      * @return string the player video code
      */
-    protected function getVideoCode()
-    {
+    protected function getVideoCode() {
         switch (strtolower($this->player)) {
-            case self::SERVICE_VIMEO:
+            case self::VIMEO:
                 $video_code = substr(parse_url($this->v, PHP_URL_PATH), 1);
                 if ($video_code)
-                     return $video_code;
+                    return $video_code;
                 break;
-            case self::SERVICE_VEOH:
+            case self::VEOH:
                 $video_code = substr(parse_url($this->v, PHP_URL_PATH), 7);
                 if ($video_code)
-                     return $video_code;
+                    return $video_code;
                 break;
             default:
-                $get_vars  = explode('&', html_entity_decode(parse_url($this->v, PHP_URL_QUERY)));
-                foreach($get_vars as $var) {
+                if($this->iframe){
+                    return $this->v;
+                }
+                
+                $get_vars = explode('&', html_entity_decode(parse_url($this->v, PHP_URL_QUERY)));
+                foreach ($get_vars as $var) {
                     $exploded_var = explode('=', $var);
                     if ($exploded_var[0] === 'v')
                         return $exploded_var[1];
@@ -329,26 +291,22 @@ CODESET;
                 break;
         }
         $this->_status = self::NO_VIDEO_CODE_IN_URL;
-        return false;    
+        return false;
     }
-    
+
     /**
-         * Current error string
-         * 
-         * That function is call render but it doesn't render anything!
      * @return string the error message to be displayed
      */
-    protected function renderError()
-    {
+    protected function renderError() {
         switch ($this->_status) {
             case self::NO_VIDEO_CODE:
-                return Yii::t('yiitube', 'you need to set a video code to use this widget.<br/>Check the online documentation.');
+                return Yii::t('yiitube', 'you need to provide a video code to display video<br/>');
                 break;
             case self::NO_VIDEO_CODE_IN_URL:
-                return Yii::t('yiitube', 'the URL you provided does not contain a youtube video code.');
+                return Yii::t('yiitube', 'the URL you provided does not contain a valid video code.');
                 break;
             case self::NO_SUPPORTED_PLAYER:
-                return Yii::t('yiitube', 'Yiitube doesn\'t support {player}', array('{player}' => $this->player));
+                return Yii::t('yiitube', '{player} is not supported.', array('{player}' => $this->player));
                 break;
         }
     }
